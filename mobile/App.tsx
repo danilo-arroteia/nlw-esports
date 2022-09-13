c1d62ff03cecc4e06aae92d2666ae8d209cd878a
Danilo Arroteia <sikor12@gmail.com>
2022-09-13 12:00:54 -0300

    finished expo start

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+interface ButtonProps{ 
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>
+        Hello NLW eSports
+      </Text>
+      <Button title='Send 1'/>
+      <Button title='Send 2'/>
+      <Button title='Send 3'/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +32,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
 });
